467cf3285248603620d6637cc73876bb0f36bf33
Swordmagus <83208846+Swordmagus@users.noreply.github.com>
2024-08-08 22:45:30 +0530

    Update CartAndWish.java

--- Assignment/src/CartAndWish.java
+++ Assignment/src/CartAndWish.java
@@ -17,6 +17,11 @@ public abstract class CartAndWish {
     public List<Product> getProducts() {
         return products;
     }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
 }
 
 class Cart extends CartAndWish {
@@ -25,6 +30,27 @@ class Cart extends CartAndWish {
         System.out.println("Cart:");
         super.viewProducts();
     }
+
+    @Override
+    public void removeProduct(Product product) {
+        System.out.println("Removing product from cart: " + product);
+        super.removeProduct(product);
+    }
+
+    public static void removeAllProductsFromCart(User currentUser) {
+        List<Product> productsInCart = currentUser.getCart().getProducts();
+
+        if (productsInCart.isEmpty()) {
+            System.out.println("Your cart is empty.");
+            return;
+        }
+        for (Product product : productsInCart) {
+            currentUser.getCart().removeProduct(product);
+        }
+        System.out.println("All products have been removed from the cart.");
+    }
+
+    
 }
 
 class Wishlist extends CartAndWish {
@@ -32,5 +58,27 @@ class Wishlist extends CartAndWish {
     public void viewProducts() {
         System.out.println("Wishlist:");
         super.viewProducts();
+    }
+
+    @Override
+    public void removeProduct(Product product) {
+        System.out.println("Removing product from wishlist: " + product);
+        super.removeProduct(product);
+    }
+
+    protected static void addAllWishlistToCart(User currentUser) {
+        List<Product> wishlist = currentUser.getWishlist().getProducts(); // get wishlist
+
+        if (wishlist.isEmpty()) {
+            System.out.println("Your wishlist is empty.");
+            return;
+        }
+        List<Product> wishlistCopy = new ArrayList<>(wishlist); // copy list
+
+        for (Product product : wishlistCopy) {
+            currentUser.addToCart(product);
+            currentUser.getWishlist().removeProduct(product);
+        }
+        System.out.println("All wishlist products have been added to the cart.\n");
     }
 }
